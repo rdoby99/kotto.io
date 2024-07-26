f951ddd8ce1476e59539f6074fd12dc4238a62e6
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-07-26 17:51:18 -0400

    Footer logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-between w-full py-4 px-4 md:px-16 text-secondary-foreground">
+    <footer className="flex justify-between items-end w-full py-4 px-4 md:px-16 text-secondary-foreground">
       <div className="flex flex-col">
+        <Image
+          src="/logo.png"
+          alt="Japanese Reader Homepage"
+          height={64}
+          width={64}
+          className="mb-4"
+        />
         <p>&copy; 2022 Japanese Reader</p>
         <p>Created by Ramona Doby</p>
       </div>
