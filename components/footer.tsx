f68fd004466582175f8230f7ae9deabb95499d1a
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-08-30 11:12:21 -0400

    refactor(footer.tsx): added github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="flex flex-col md:flex-row justify-between items-center md:items-end w-full py-4 px-4 md:px-16 text-secondary-foreground">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col">
         <Image
           src="/logo.png"
           alt="Japanese Reader Homepage"
@@ -12,6 +12,13 @@ export default function Footer() {
           width={64}
           className="mb-4"
         />
+        <a
+          className="hover:opacity-50 font-bold"
+          target="_blank"
+          href="https://github.com/rdoby99/kotto.io"
+        >
+          View on Github
+        </a>
         <p>&copy; 2024 Ramona Doby</p>
       </div>
       <p className="text-xs">
