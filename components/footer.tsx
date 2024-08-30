91a6ed45d923fccda25c8626adcc002497d74d32
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-08-30 11:32:03 -0400

    fix(footer.tsx): footer logo alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="flex flex-col md:flex-row justify-between items-center md:items-end w-full py-4 px-4 md:px-16 text-secondary-foreground">
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center md:items-start">
         <Image
           src="/logo.png"
           alt="Japanese Reader Homepage"
